316a7e6c568e0155c75bec4f4f1b4a60573a0558
quattromani <max@maxquattromani.com>
2015-05-08 10:02:50 -0600

    updates & improvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,198 +1,210 @@
 module.exports = function(grunt) {
 
-  require('time-grunt')(grunt);
-  require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
 
-    config: {
-      src: 'src',
-      dist: 'dist'
+  	// Project configuration
+  	config: {
+  		src: 'src',
+  		dist: 'dist'
+  	},
+
+    // SCSS to CSS
+    sass: {
+    	dist: {
+    		options: {
+    			style: 'expanded'
+    		},
+    		files: {
+    			'<%= config.src %>/css/main.css': '<%= config.src %>/css/scss/main.scss'
+    		}
+    	}
     },
 
-    // Takes your scss files and compiles them to css
-    sass: {
+    // Clean up and remove any unused CSS - prepare for inlining
+    uncss: {
       dist: {
+        src: ['<%= config.src %>/index.html'],
+        dest: '<%= config.dist %>/css/tidy.css',
         options: {
-          style: 'expanded'
-        },
-        files: {
-          'src/css/main.css': 'src/css/scss/main.scss'
+        	ignoreSheets : [/fonts.googleapis/],
+          report: 'gzip'
         }
       }
     },
 
+    // Recompile to /dist folder
+    processhtml: {
+		  dist: {
+		    files: {
+		      '<%= config.dist %>/index.html': ['<%= config.src %>/index.html']
+		    }
+		  }
+		},
+
+		// Setup a localhost & start livereload
     connect: {
-      options: {
-        port: 9000,
-        livereload: 35729,
+    	options: {
+    		port: 9000,
+    		livereload: 35729,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
       livereload: {
-        options: {
-          open: true,
-          base: [
-          'dist'
-          ]
-        }
+      	options: {
+      		open: true,
+      		base: ['dist']
+      	}
       }
     },
 
     // Assembles your email content with html layout
     assemble: {
-      options: {
-        layoutdir: 'src/layouts',
-        flatten: true
-      },
-      pages: {
-        src: ['src/emails/*.hbs'],
-        dest: 'dist/'
-      }
-    },
+    	options: {
+    		flatten: true,
+    		layoutdir: '<%= config.src %>/layouts',
+    		data: '<%= config.src %>/data/*.{json,yml}',
+    		partials: '<%= config.src %>/partials/{,*/}*.hbs'
+    	},
+	    pages: {
+	    	src: ['<%= config.src %>/emails/*.hbs'],
+	    	dest: '<%= config.src %>/'
+	    }
+	  },
 
-    // Inlines your css
+    // Inline CSS and create a .txt version
     premailer: {
-      html: {
-        options: {
-          removeComments: true
-        },
-        files: [{
-          expand: true,
-          src: ['dist/*.html'],
-          dest: ''
-        }]
-      },
-      txt: {
-        options: {
-          mode: 'txt'
-        },
-        files: [{
-          expand: true,
-          src: ['dist/*.html'],
-          dest: '',
-          ext: '.txt'
-        }]
-      }
+    	html: {
+    		options: {
+    			removeComments: true
+    		},
+    		files: [{
+    			expand: true,
+    			src: ['<%= config.dist %>/*.html'],
+    			dest: ''
+    		}]
+    	},
+    	txt: {
+    		options: {
+    			mode: 'txt'
+    		},
+    		files: [{
+    			expand: true,
+    			src: ['<%= config.dist %>/*.html'],
+    			dest: '',
+    			ext: '.txt'
+    		}]
+    	}
     },
 
-    // Watches for changes to css or email templates then runs grunt tasks
-    // watch: {
-    //   files: ['src/css/scss/*','src/emails/*','src/layouts/*'],
-    //   tasks: ['build']
-    // },
-
+    // Watch for changes and fire livereload
     watch: {
-      assemble: {
-        files: ['src/css/scss/*','src/emails/*','src/layouts/*'],
-        tasks: ['build']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-        files: [
-          'dist/{,*/}*.html'
-        ]
-      }
-    },
+    	assemble: {
+    		files: [
+    			'<%= config.src %>/css/scss/{,*/}*.scss',
+    			'<%= config.src %>/emails/*',
+    			'<%= config.src %>/layouts/*',
+    			'<%= config.src %>/partials/*'
+    		],
+    		tasks: ['email']
+    	},
+    	livereload: {
+    		options: {
+    			livereload: '<%= connect.options.livereload %>'
+    		},
+    		files: ['<%= config.dist %>/{,*/}*.html']
+  		}
+		},
 
     // Use Mailgun option if you want to email the design to your inbox or to something like Litmus
     // grunt send --template=transaction.html
     mailgun: {
-      mailer: {
-        options: {
+    	mailer: {
+    		options: {
           key: 'key-5303e1f6ac9d0c9bcb55c488b40d50d9', // Enter your Mailgun API key here
-          sender: 'max@qoppa.co', // Change this
-          recipient: 'max@qoppa.co', // Change this
-          subject: 'This is a test email'
+          sender: 'max@placewise.com', // Change this
+          recipient: 'max@placewise.com', // Change this
+          subject: 'This is it!'
         },
-        src: ['dist/'+grunt.option('template')]
+        src: ['<%= config.dist %>/'+grunt.option('template')]
       }
     },
 
-    // Use Rackspace Cloud Files if you're using images in your email
-    cloudfiles: {
-      prod: {
-        'user': 'Rackspace Cloud Username', // Change this
-        'key': 'Rackspace Cloud API Key', // Change this
-        'region': 'ORD', // Might need to change this
-        'upload': [{
-          'container': 'Files Container Name', // Change this
-          'src': 'src/img/*',
-          'dest': '/',
-          'stripcomponents': 0
-        }]
-      }
-    },
-
-    // CDN will replace local paths with your Cloud CDN path
-    cdn: {
-      options: {
-        cdn: 'Rackspace Cloud CDN URI', // Change this
-        flatten: true,
-        supportedTypes: 'html'
-      },
-      dist: {
-        src: ['./dist/*.html']
-      }
-    },
+    // Deploy Images to server
+  	// 'sftp-deploy': {
+		//    build: {
+		//     auth: {
+		//       host: 'server.com',
+		//       port: 22,
+		//       authKey: 'key1'
+		//     },
+		//     cache: 'sftpCache.json',
+		//     src: '/path/to/source/folder',
+		//     dest: '/path/to/destination/folder',
+		//     exclusions: ['/path/to/source/folder/**/.DS_Store', '/path/to/source/folder/**/Thumbs.db', 'dist/tmp'],
+		//     serverSep: '/',
+		//     concurrency: 4,
+		//     progress: true
+		//   }
+		// }
 
     // Send your email template to Litmus for testing
-    // grunt litmus --template=transaction.html
     litmus: {
-      test: {
-        src: ['dist/'+grunt.option('template')],
-        options: {
-          username: 'username', // Change this
-          password: 'password', // Change this
-          url: 'https://yourcompany.litmus.com', // Change this
+    	test: {
+    		src: ['dist/index.html'],
+    		options: {
+          username: 'mallfinder', // Change this
+          password: 'pascalswager', // Change this
+          url: 'https://mfnetwork.litmus.com', // Change this
           clients: ['android4', 'aolonline', 'androidgmailapp', 'aolonline', 'ffaolonline',
-          'chromeaolonline', 'appmail6', 'iphone6', 'ipadmini', 'ipad', 'chromegmailnew',
+          'chromeaolonline', 'appmail6', 'iphone4', 'iphone5', 'iphone6', 'ipadmini', 'ipad', 'chromegmailnew',
           'iphone6plus', 'notes85', 'ol2002', 'ol2003', 'ol2007', 'ol2010', 'ol2011',
           'ol2013', 'outlookcom', 'chromeoutlookcom', 'chromeyahoo', 'windowsphone8'] // https://#{company}.litmus.com/emails/clients.xml
         }
       }
     },
 
+    // Copy img files from src to dist
     copy: {
-      images: {
-        expand: true,
-        cwd: 'src/images',
-        src: '**',
-        dest: 'dist/images'
-      }
+    	main: {
+    		expand: true,
+    		cwd: '<%= config.src %>/img',
+    		src: '**',
+    		dest: '<%= config.dist %>/',
+    		flatten: true,
+    		filter: 'isFile',
+    	},
     },
 
     // remove any previously-created files.
-    clean: ['dist/**/*.{html,txt,xml}']
+    clean: ['<%= config.dist %>/**/*.{html,txt,xml}']
 
   });
 
-  grunt.loadNpmTasks('assemble');
+	grunt.loadNpmTasks('assemble');
 
-  grunt.registerTask('server', [
-    'build',
-    'assemble',
-    'premailer',
-    'connect:livereload',
-    'watch'
-    ]);
+	grunt.registerTask('email', [
+		'clean',
+		'sass',
+		'assemble',
+		'copy',
+		'uncss:dist',
+		'processhtml',
+		'assemble',
+		'premailer',
+		'connect:livereload',
+		'watch'
+	]);
 
-  grunt.registerTask('build', [
-    'sass',
-    'clean',
-    'copy',
-    'assemble',
-    'premailer'
-    ]);
+	// Compile and load up your email
+	grunt.registerTask('default', ['email']);
 
-  grunt.registerTask('default', [
-    'build'
-    ]);
+ 	// Uses `grunt send --template=index.html`
+ 	grunt.registerTask('send', ['mailgun']);
 
-  // Use `grunt send --template=index.html`
-    grunt.registerTask('send', ['mailgun']);
+ 	// Uses `grunt litmus --template=index.html`
+ 	grunt.registerTask('test', ['litmus']);
 
 };
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
 # Responsive Email Design Workflow
 
+Email Generator using INK and Grunt
+
+- 'git clone'
+- 'npm install'
+- 'grunt email'
+
+===================================
+
+
+
+Simply run 'grunt email' and it will generate your html responsive email template with inline styles.
+
+Run 'grunt send --template=index.html' to send test email
+
+*Warning: I have modified Ink files to match our current web build process - Use caution if/when updating*
+
+@ http://zurb.com/ink/
